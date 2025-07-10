45a4bf4c9f1708c52f0bfad7e07d00ced343311b
Håvard Bakke <havard.bakke@pexip.com>
2025-07-10 14:00:02 +0200

    Refactor STUN server resource configuration for improved readability

--- testdata/resource_infinity_stun_server_basic_updated/resources.tf
+++ testdata/resource_infinity_stun_server_basic_updated/resources.tf
@@ -1,6 +1,6 @@
 resource "pexip_infinity_stun_server" "stun-server-test" {
-  name           = "stun-server-test"
-  description    = "Test STUN server"
-  address        = "test-stun-server.dev.pexip.network"
-  port           = 8081 // Updated port number
+  name        = "stun-server-test"
+  description = "Test STUN server"
+  address     = "test-stun-server.dev.pexip.network"
+  port        = 8081 // Updated port number
 }
