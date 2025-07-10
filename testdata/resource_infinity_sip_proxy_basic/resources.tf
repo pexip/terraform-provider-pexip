a41d05347d6dadd34f5dbbef913eff3eb0b0c25d
Håvard Bakke <havard.bakke@pexip.com>
2025-07-10 13:25:06 +0200

    Refactor SIP proxy resource configuration for improved readability

--- testdata/resource_infinity_sip_proxy_basic/resources.tf
+++ testdata/resource_infinity_sip_proxy_basic/resources.tf
@@ -1,7 +1,7 @@
 resource "pexip_infinity_sip_proxy" "sip-proxy-test" {
-  name                    = "test-sip-proxy"
-  description             = "Test SIP Proxy"
-  address                 = "test-sip-proxy.dev.pexip.network"
-  port                    = 8080
-  transport               = "tcp"
+  name        = "test-sip-proxy"
+  description = "Test SIP Proxy"
+  address     = "test-sip-proxy.dev.pexip.network"
+  port        = 8080
+  transport   = "tcp"
 }
