a0e9f00ab52e7dc4bc7cdd565112574d01680286
Mike Assel <mike.assel@pexip.com>
2025-07-11 10:05:22 -0400

    add depends_on to tls-cert-test

--- example/gcp-infinity-manager/infinity_config.tf
+++ example/gcp-infinity-manager/infinity_config.tf
@@ -44,4 +44,21 @@ resource "pexip_infinity_system_location" "AMS" {
 resource "pexip_infinity_tls_certificate" "tls-cert-test" {
   certificate = tls_self_signed_cert.manager_cert.cert_pem
   private_key = tls_private_key.manager_private_key.private_key_pem
+
+  depends_on = [
+    google_compute_instance.infinity_manager,
+    null_resource.wait_for_infinity_manager_http
+  ]
 }
+
+/*
+resource "pexip_infinity_tls_certificate" "tls-cert-test2" {
+  certificate = file("gcp-infinity-manager/test-cert.pem")
+  private_key = file("gcp-infinity-manager/test-key.key")
+
+  depends_on = [
+    google_compute_instance.infinity_manager,
+    null_resource.wait_for_infinity_manager_http
+  ]
+}
+*/
