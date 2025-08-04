708ab8d9d6dfc32624b4972ef1149146e397670c
Håvard Bakke <havard.bakke@pexip.com>
2025-08-04 15:19:54 +0200

    Update Pexip provider source and enhance system location configuration

--- internal/provider/resource_infinity_system_location_test.go
+++ internal/provider/resource_infinity_system_location_test.go
@@ -32,31 +32,45 @@ func TestInfinitySystemLocation(t *testing.T) {
 	client.On("GetJSON", mock.Anything, "configuration/v1/system_location/123/", mock.Anything).Return(nil).Run(func(args mock.Arguments) {
 		sysLoc := args.Get(2).(*config.SystemLocation)
 		if updated {
+			mediaQoS := 0
+			signallingQoS := 0
 			*sysLoc = config.SystemLocation{
-				ID:          123,
-				Name:        "main",
-				Description: "Main location for Pexip Infinity System - updated",
-				MTU:         1460,
+				ID:            123,
+				Name:          "main",
+				Description:   "Main location for Pexip Infinity System - updated",
+				MTU:           1460,
+				MediaQoS:      &mediaQoS,
+				SignallingQoS: &signallingQoS,
 				DNSServers: []config.DNSServer{
 					{ID: 1, ResourceURI: "/api/admin/configuration/v1/dns_server/1/"},
 				},
 				NTPServers: []config.NTPServer{
 					{ID: 1, ResourceURI: "/api/admin/configuration/v1/ntp_server/1/"},
 				},
+				ClientSTUNServers: []config.STUNServer{
+					{ID: 2},
+				},
 				ResourceURI: "/api/admin/configuration/v1/system_location/123/",
 			}
 		} else {
+			mediaQoS := 0
+			signallingQoS := 0
 			*sysLoc = config.SystemLocation{
-				ID:          123,
-				Name:        "main",
-				Description: "Main location for Pexip Infinity System",
-				MTU:         1460,
+				ID:            123,
+				Name:          "main",
+				Description:   "Main location for Pexip Infinity System",
+				MTU:           1460,
+				MediaQoS:      &mediaQoS,
+				SignallingQoS: &signallingQoS,
 				DNSServers: []config.DNSServer{
 					{ID: 1, ResourceURI: "/api/admin/configuration/v1/dns_server/1/"},
 					{ID: 2, ResourceURI: "/api/admin/configuration/v1/dns_server/2/"},
 				},
 				NTPServers: []config.NTPServer{
 					{ID: 1, ResourceURI: "/api/admin/configuration/v1/ntp_server/1/"},
+				},
+				ClientSTUNServers: []config.STUNServer{
+					{ID: 1},
 				},
 				ResourceURI: "/api/admin/configuration/v1/system_location/123/",
 			}
@@ -69,16 +83,23 @@ func TestInfinitySystemLocation(t *testing.T) {
 	}), mock.Anything, mock.Anything).Return(nil).Run(func(args mock.Arguments) {
 		updated = true // Mark as updated for subsequent reads
 		sysLoc := args.Get(3).(*config.SystemLocation)
+		mediaQoS := 0
+		signallingQoS := 0
 		*sysLoc = config.SystemLocation{
-			ID:          123,
-			Name:        "main",
-			Description: "Main location for Pexip Infinity System - updated",
-			MTU:         1460,
+			ID:            123,
+			Name:          "main",
+			Description:   "Main location for Pexip Infinity System - updated",
+			MTU:           1460,
+			MediaQoS:      &mediaQoS,
+			SignallingQoS: &signallingQoS,
 			DNSServers: []config.DNSServer{
 				{ID: 1, ResourceURI: "/api/admin/configuration/v1/dns_server/1/"},
 			},
 			NTPServers: []config.NTPServer{
 				{ID: 1, ResourceURI: "/api/admin/configuration/v1/ntp_server/1/"},
+			},
+			ClientSTUNServers: []config.STUNServer{
+				{ID: 2},
 			},
 			ResourceURI: "/api/admin/configuration/v1/system_location/123/",
 		}
@@ -106,7 +127,9 @@ func testInfinitySystemLocation(t *testing.T, client InfinityClient) {
 					resource.TestCheckResourceAttr("pexip_infinity_system_location.main-location", "mtu", "1460"),
 					resource.TestCheckResourceAttr("pexip_infinity_system_location.main-location", "dns_servers.#", "2"),
 					resource.TestCheckTypeSetElemAttr("pexip_infinity_system_location.main-location", "dns_servers.*", "/api/admin/configuration/v1/dns_server/1/"),
-					resource.TestCheckTypeSetElemAttr("pexip_infinity_system_location.main-location", "dns_servers.*", "/api/admin/configuration/v1/dns_server/2/")),
+					resource.TestCheckTypeSetElemAttr("pexip_infinity_system_location.main-location", "dns_servers.*", "/api/admin/configuration/v1/dns_server/2/"),
+					resource.TestCheckResourceAttr("pexip_infinity_system_location.main-location", "client_stun_servers.#", "1"),
+					resource.TestCheckTypeSetElemAttr("pexip_infinity_system_location.main-location", "client_stun_servers.*", "/api/admin/configuration/v1/stun_server/1/")),
 			},
 			{
 				Config: test.LoadTestFolder(t, "resource_infinity_system_location_basic_updated"),
@@ -118,6 +141,8 @@ func testInfinitySystemLocation(t *testing.T, client InfinityClient) {
 					resource.TestCheckResourceAttr("pexip_infinity_system_location.main-location", "mtu", "1460"),
 					resource.TestCheckResourceAttr("pexip_infinity_system_location.main-location", "dns_servers.#", "1"),
 					resource.TestCheckTypeSetElemAttr("pexip_infinity_system_location.main-location", "dns_servers.*", "/api/admin/configuration/v1/dns_server/1/"),
+					resource.TestCheckResourceAttr("pexip_infinity_system_location.main-location", "client_stun_servers.#", "1"),
+					resource.TestCheckTypeSetElemAttr("pexip_infinity_system_location.main-location", "client_stun_servers.*", "/api/admin/configuration/v1/stun_server/2/"),
 				),
 			},
 		},
