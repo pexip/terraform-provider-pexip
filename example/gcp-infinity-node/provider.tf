25d187dce0122b0922ec49ced56d944312a5a773
Håvard Bakke <habakke@users.noreply.github.com>
2025-08-06 22:32:44 +0200

    Add additional attributes to the system_location resource (#6)
    * refactor: update system location resources and improve configuration parameters
    
    * style: standardize formatting in system_location resources
    
    * chore: update go-infinity-sdk to version 38.0.5
    
    * fix: update event sink ResourceURI in system location test
    
    * fix: add 'signalling' to ignore-rules in golangci configuration
    
    * fix: update pexip provider version constraint to exact version 0.0.1

--- example/gcp-infinity-node/provider.tf
+++ example/gcp-infinity-node/provider.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     pexip = {
       source  = "pexip.com/pexip/pexip"
-      version = ">= 1.0"
+      version = "0.0.1"
     }
     google = {
       source  = "hashicorp/google"
--- testdata/resource_infinity_system_location_basic_updated/resources.tf
+++ testdata/resource_infinity_system_location_basic_updated/resources.tf
@@ -11,4 +11,7 @@ resource "pexip_infinity_system_location" "main-location" {
   dns_servers         = ["/api/admin/configuration/v1/dns_server/1/"] # Updated DNS servers
   ntp_servers         = ["/api/admin/configuration/v1/ntp_server/1/"]
   client_stun_servers = ["/api/admin/configuration/v1/stun_server/2/"] # Updated STUN server
+  client_turn_servers = ["/api/admin/configuration/v1/turn_server/2/"] # Updated TURN server
+  event_sinks         = ["/api/admin/configuration/v1/event_sink/1/"]  # Updated event sink
+  stun_server         = "/api/admin/configuration/v1/stun_server/2/"
 }
