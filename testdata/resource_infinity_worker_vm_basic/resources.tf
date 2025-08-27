3380ade9e8249f397c0ef208c9d17386a5469e24
Håvard Bakke <havard.bakke@pexip.com>
2025-08-27 20:28:33 +0200

    feat: add TLS certificate and public IP configuration for Infinity Node

--- testdata/resource_infinity_worker_vm_basic/resources.tf
+++ testdata/resource_infinity_worker_vm_basic/resources.tf
@@ -4,19 +4,33 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-resource "pexip_infinity_worker_vm" "worker_vm-test" {
-  name                    = "worker_vm-test"
-  hostname                = "worker_vm-test"
-  domain                  = "test-value"
-  address                 = "192.168.1.10"
-  netmask                 = "255.255.255.0"
-  gateway                 = "192.168.1.1"
-  ipv6_address            = "2001:db8::1"
-  ipv6_gateway            = "2001:db8::fe"
-  node_type               = "CONFERENCING"
-  transcoding             = true
-  password                = "test-value"
-  maintenance_mode        = true
-  maintenance_mode_reason = "test-value"
-  system_location         = "test-value"
+resource "pexip_infinity_worker_vm" "worker-vm-test" {
+  name                          = "worker-vm-test"
+  hostname                      = "worker-vm-test"
+  domain                        = "test-value"
+  address                       = "192.168.1.10"
+  netmask                       = "255.255.255.0"
+  gateway                       = "192.168.1.1"
+  system_location               = "/api/admin/configuration/v1/system_location/1/"
+  ipv6_address                  = "2001:db8::1"
+  ipv6_gateway                  = "2001:db8::fe"
+  description                   = "initial description"
+  snmp_system_contact           = "snmpcontact1@domain.com"
+  snmp_system_location          = "eu1"
+  snmp_username                 = "snmp-user1"
+  snmp_authentication_password  = "auth-password1"
+  snmp_privacy_password         = "privacy-password1"
+  snmp_mode                     = "STANDARD"
+  enable_ssh                    = "ON"
+  enable_distributed_database   = true
+  media_priority_weight         = 0
+  cloud_bursting                = false
+  ssh_authorized_keys_use_cloud = true
+  tls_certificate               = "/api/admin/configuration/v1/tls_certificate/2/"
+  static_nat_address            = "203.0.113.2"
+  //node_type               = "CONFERENCING"
+  //transcoding             = true
+  //password                = "test-value"
+  //maintenance_mode        = true
+  //maintenance_mode_reason = "test-value"
 }
