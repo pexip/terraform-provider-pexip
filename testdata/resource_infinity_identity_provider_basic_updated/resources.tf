1fa239db81327a327ab63cc278af3841ea3368bc
Håvard Bakke <havard.bakke@pexip.com>
2025-12-12 14:56:40 +0100

    feat: update identity provider resource to enhance OIDC support and improve attribute handling

--- testdata/resource_infinity_identity_provider_basic_updated/resources.tf
+++ testdata/resource_infinity_identity_provider_basic_updated/resources.tf
@@ -5,33 +5,28 @@
  */
 
 resource "pexip_infinity_identity_provider" "identity_provider-test" {
-  name                                     = "identity_provider-test"
-  description                              = "Updated Test IdentityProvider" // Updated description
-  idp_type                                 = "oidc"                          // Updated value
-  sso_url                                  = "https://updated.example.com"   // Updated URL
-  idp_entity_id                            = "updated-value"                 // Updated value
-  idp_public_key                           = "updated-value"                 // Updated value
-  service_entity_id                        = "updated-value"                 // Updated value
-  service_public_key                       = "updated-value"                 // Updated value
-  service_private_key                      = "updated-value"                 // Updated value
-  signature_algorithm                      = "rsa-sha1"                      // Updated value
-  digest_algorithm                         = "sha1"                          // Updated value
-  display_name_attribute_name              = "identity_provider-test"
-  registration_alias_attribute_name        = "identity_provider-test"
-  assertion_consumer_service_url           = "https://updated.example.com" // Updated URL
-  worker_fqdn_acs_urls                     = false                         // Updated to false
-  disable_popup_flow                       = false                         // Updated to false
-  oidc_flow                                = "implicit"                    // Updated value
-  oidc_client_id                           = "updated-value"               // Updated value
-  oidc_client_secret                       = "updated-value"               // Updated value
-  oidc_token_url                           = "https://updated.example.com" // Updated URL
-  oidc_user_info_url                       = "https://updated.example.com" // Updated URL
-  oidc_jwks_url                            = "https://updated.example.com" // Updated URL
-  oidc_token_endpoint_auth_scheme          = "client_secret_post"          // Updated value
-  oidc_token_signature_scheme              = "hs256"                       // Updated value
-  oidc_display_name_claim_name             = "identity_provider-test"
-  oidc_registration_alias_claim_name       = "identity_provider-test"
-  oidc_additional_scopes                   = "updated-value" // Updated value
-  oidc_france_connect_required_eidas_level = "eidas2"        // Updated value
-  attributes                               = "updated-value" // Updated value
+  name                                     = "identity_provider-test-updated"
+  description                              = "Updated Test IdentityProvider"
+  idp_type                                 = "oidc"
+  sso_url                                  = "https://updated.example.com/sso"
+  idp_entity_id                            = "https://updated.example.com/entity"
+  service_entity_id                        = "https://pexip-updated.example.com/entity"
+  signature_algorithm                      = "http://www.w3.org/2000/09/xmldsig#rsa-sha1"
+  digest_algorithm                         = "http://www.w3.org/2001/04/xmlenc#sha1"
+  display_name_attribute_name              = "name"
+  registration_alias_attribute_name        = "username"
+  worker_fqdn_acs_urls                     = false
+  disable_popup_flow                       = false
+  oidc_flow                                = "implicit"
+  oidc_client_id                           = "updated-client-id"
+  oidc_client_secret                       = "updated-client-secret"
+  oidc_token_url                           = "https://updated.example.com/token"
+  oidc_user_info_url                       = "https://updated.example.com/userinfo"
+  oidc_jwks_url                            = "https://updated.example.com/jwks"
+  oidc_token_endpoint_auth_scheme          = "client_secret_post"
+  oidc_token_signature_scheme              = "hs256"
+  oidc_display_name_claim_name             = "name"
+  oidc_registration_alias_claim_name       = "preferred_username"
+  oidc_additional_scopes                   = "profile email"
+  oidc_france_connect_required_eidas_level = "eidas2"
 }
