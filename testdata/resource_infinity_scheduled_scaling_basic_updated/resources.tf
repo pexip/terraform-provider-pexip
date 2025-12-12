6b8c5a636f6435b78c04b9b873d5b1939c601894
Håvard Bakke <havard.bakke@pexip.com>
2025-12-12 14:01:35 +0100

    fix: update scheduled scaling resource to use TeamsConnectorScaling policy type and adjust related attributes

--- testdata/resource_infinity_scheduled_scaling_basic_updated/resources.tf
+++ testdata/resource_infinity_scheduled_scaling_basic_updated/resources.tf
@@ -6,20 +6,20 @@
 
 resource "pexip_infinity_scheduled_scaling" "scheduled_scaling-test" {
   policy_name         = "scheduled_scaling-test"
-  policy_type         = "management_vm" // Updated value
-  resource_identifier = "updated-value" // Updated value
-  enabled             = false           // Updated to false
-  local_timezone      = "updated-value" // Updated value
-  start_date          = "updated-value" // Updated value
-  time_from           = "updated-value" // Updated value
-  time_to             = "updated-value" // Updated value
-  instances_to_add    = 3               // Updated value
-  minutes_in_advance  = 30              // Updated value
-  mon                 = false           // Updated to false
-  tue                 = false           // Updated to false
-  wed                 = false           // Updated to false
-  thu                 = false           // Updated to false
-  fri                 = false           // Updated to false
-  sat                 = false           // Updated to false
-  sun                 = false           // Updated to false
+  policy_type         = "TeamsConnectorScaling"
+  resource_identifier = "updated-value"   // Updated value
+  enabled             = false              // Updated to false
+  local_timezone      = "America/New_York" // Updated value
+  start_date          = "2024-02-01"       // Updated value
+  time_from           = "08:00"            // Updated value
+  time_to             = "18:00"            // Updated value
+  instances_to_add    = 3                  // Updated value
+  minutes_in_advance  = 30                 // Updated value
+  mon                 = false              // Updated to false
+  tue                 = false              // Updated to false
+  wed                 = false              // Updated to false
+  thu                 = false              // Updated to false
+  fri                 = false              // Updated to false
+  sat                 = false              // Updated to false
+  sun                 = false              // Updated to false
 }
