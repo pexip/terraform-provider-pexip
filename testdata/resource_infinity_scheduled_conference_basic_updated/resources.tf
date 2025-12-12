d8b80b41f3ab1317137c44c14e9a295900d63eed
Håvard Bakke <havard.bakke@pexip.com>
2025-12-12 13:16:33 +0100

    fix: adjust formatting of scheduled conference resource attributes

--- testdata/resource_infinity_scheduled_conference_basic_updated/resources.tf
+++ testdata/resource_infinity_scheduled_conference_basic_updated/resources.tf
@@ -12,11 +12,11 @@ resource "pexip_infinity_conference" "test-conference" {
 
 resource "pexip_infinity_scheduled_conference" "scheduled_conference-test" {
   conference   = pexip_infinity_conference.test-conference.id
-  start_time   = "2024-02-01T14:00:00Z" // Updated value
-  end_time     = "2024-02-01T15:30:00Z" // Updated value
+  start_time   = "2024-02-01T14:00:00Z"              // Updated value
+  end_time     = "2024-02-01T15:30:00Z"              // Updated value
   subject      = "Updated Test Scheduled Conference" // Updated value
-  ews_item_id  = "updated-ews-item-id" // Updated value
-  ews_item_uid = "updated-ews-item-uid" // Updated value
+  ews_item_id  = "updated-ews-item-id"               // Updated value
+  ews_item_uid = "updated-ews-item-uid"              // Updated value
 
   depends_on = [
     pexip_infinity_conference.test-conference
