bb6d7eb5d06763d6fb39110b6901b0d18297afd5
Håvard Bakke <havard.bakke@pexip.com>
2025-12-12 11:17:32 +0100

    fix: update automatic participant configuration in resources.tf

--- testdata/resource_infinity_automatic_participant_basic_updated/resources.tf
+++ testdata/resource_infinity_automatic_participant_basic_updated/resources.tf
@@ -20,12 +20,12 @@ resource "pexip_infinity_automatic_participant" "automatic-participant-test" {
   alias                 = "automatic-participant-updated"     // Updated value
   description           = "Updated Test AutomaticParticipant" // Updated description
   conference            = [pexip_infinity_conference.test-conference.id]
-  protocol              = "h323"                      // Updated value
-  call_type             = "audio"                     // Updated value
-  role                  = "chair"                     // Updated value
-  dtmf_sequence         = "456*"                      // Updated value
+  protocol              = "h323"                  // Updated value
+  call_type             = "audio"                 // Updated value
+  role                  = "chair"                 // Updated value
+  dtmf_sequence         = "456*"                  // Updated value
   keep_conference_alive = "keep_conference_alive" // Keep same value as API rejects end_conference_when_alone
-  routing               = "manual"                    // Updated value
+  routing               = "manual"                // Updated value
   system_location       = pexip_infinity_system_location.test-location.id
   streaming             = false // Updated to false
   remote_display_name   = "automatic_participant-test"
