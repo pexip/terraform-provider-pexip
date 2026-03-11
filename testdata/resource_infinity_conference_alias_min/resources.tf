f51dfa07088ef539a61fa23550d0a016ff1d0681
Mike Assel <mike.assel@pexip.com>
2026-03-11 12:52:18 -0400

    min/full tests conference_alias

---
+++ testdata/resource_infinity_conference_alias_min/resources.tf
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Pexip AS
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+resource "pexip_infinity_conference" "tf-test-conference" {
+  name         = "tf-test-conference"
+  description  = "Test Conference"
+  service_type = "conference"
+}
+
+resource "pexip_infinity_conference_alias" "tf-test-conference-alias" {
+  alias      = "tf-test-alias"
+  conference = pexip_infinity_conference.tf-test-conference.id
+}
