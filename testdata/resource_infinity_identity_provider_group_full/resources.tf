f5ff56bd7b9bbf2759d8ca7c53ff37a9f5232007
Mike Assel <mike.assel@pexip.com>
2026-03-11 13:39:55 -0400

    min/full tests identity_group_provider

---
+++ testdata/resource_infinity_identity_provider_group_full/resources.tf
@@ -0,0 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Pexip AS
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+resource "pexip_infinity_identity_provider_group" "tf-test-identity-provider-group" {
+  name        = "tf-test-identity-provider-group"
+  description = "Test Identity Provider Group Description"
+}
---
+++ testdata/resource_infinity_identity_provider_group_min/resources.tf
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Pexip AS
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+resource "pexip_infinity_identity_provider_group" "tf-test-identity-provider-group" {
+  name = "tf-test-identity-provider-group"
+}
