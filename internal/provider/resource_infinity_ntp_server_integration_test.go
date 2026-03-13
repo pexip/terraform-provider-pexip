c723c84ec327c3feab192d09da625bd411876ffe
Mike Assel <mike.assel@pexip.com>
2026-03-13 13:35:18 -0400

    min/full tests ntp_server

---
+++ internal/provider/resource_infinity_ntp_server_integration_test.go
@@ -0,0 +1,45 @@
+//go:build integration
+
+/*
+ * SPDX-FileCopyrightText: 2025 Pexip AS
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package provider
+
+import (
+	"crypto/tls"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/pexip/terraform-provider-pexip/internal/test"
+
+	"github.com/pexip/go-infinity-sdk/v38"
+)
+
+func TestInfinityNTPServerIntegration(t *testing.T) {
+	_ = os.Setenv("TF_ACC", "1")
+
+	client, err := infinity.New(
+		infinity.WithBaseURL(test.INFINITY_BASE_URL),
+		infinity.WithBasicAuth(test.INFINITY_USERNAME, test.INFINITY_PASSWORD),
+		infinity.WithMaxRetries(2),
+		infinity.WithTransport(&http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true, // We need this because default certificate is not trusted
+				MinVersion:         tls.VersionTLS12,
+			},
+			MaxIdleConns:        30,
+			MaxIdleConnsPerHost: 5,
+			IdleConnTimeout:     60 * time.Second,
+		}),
+	)
+	require.NoError(t, err)
+
+	testInfinityNTPServer(t, client)
+}
