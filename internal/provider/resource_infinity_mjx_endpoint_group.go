91706653c18c9dd77e4193aac2f1fc2f197a4a20
Mike Assel <mike.assel@pexip.com>
2026-03-28 19:43:18 -0400

    add plan modifiers for state management in MJX endpoint and endpoint group resources

--- internal/provider/resource_infinity_mjx_endpoint_group.go
+++ internal/provider/resource_infinity_mjx_endpoint_group.go
@@ -15,7 +15,10 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int32planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
@@ -68,10 +71,16 @@ func (r *InfinityMjxEndpointGroupResource) Schema(ctx context.Context, req resou
 			"id": schema.StringAttribute{
 				Computed:            true,
 				MarkdownDescription: "Resource URI for the MJX endpoint group in Infinity.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
 			},
 			"resource_id": schema.Int32Attribute{
 				Computed:            true,
 				MarkdownDescription: "The resource integer identifier for the MJX endpoint group in Infinity.",
+				PlanModifiers: []planmodifier.Int32{
+					int32planmodifier.UseStateForUnknown(),
+				},
 			},
 			"name": schema.StringAttribute{
 				Required: true,
