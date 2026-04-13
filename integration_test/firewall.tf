7c275d0d7d0fc8c84d91bff0bad53fa20bfe3ccb
Mike Assel <mike.assel@pexip.com>
2026-04-13 13:29:17 -0400

    Update test ci (#157)
    * feat: add Terraform setup step to CI workflow
    
    * fix: remove scheduled trigger from integration tests workflow
    
    * refactor: remove unused firewall rules and clean up integration test files
    
    * chore: update Terraform version to 1.14 in CI workflow

--- integration_test/firewall.tf
+++ integration_test/firewall.tf
@@ -4,19 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-resource "google_compute_firewall" "allow_ssh" {
-  name    = "allow-ssh-${var.project_id}"
-  network = data.google_compute_network.default.name
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_ranges = ["35.235.240.0/20"] # Allow SSH from GCP console
-  target_tags   = ["allow-ssh-${var.project_id}"]
-}
-
 resource "google_compute_firewall" "allow_https" {
   name    = "allow-https-${var.project_id}"
   network = data.google_compute_network.default.name
@@ -28,21 +15,4 @@ resource "google_compute_firewall" "allow_https" {
 
   source_ranges = ["0.0.0.0/0"] # Allow from anywhere
   target_tags   = ["allow-https-${var.project_id}"]
-}
-
-resource "google_compute_firewall" "allow_inter_node" {
-  name    = "allow-inter-node-${var.project_id}"
-  network = data.google_compute_network.default.name
-
-  allow {
-    protocol = "udp"
-    ports    = ["500"]
-  }
-
-  allow {
-    protocol = "esp"
-  }
-
-  source_ranges = [data.google_compute_subnetwork.default.ip_cidr_range]
-  target_tags   = ["allow-inter-node-${var.project_id}"]
 }
--- integration_test/main.tf
+++ integration_test/main.tf
@@ -22,9 +22,7 @@ module "gcp-infinity-manager" {
   private_subnetwork_id = data.google_compute_subnetwork.default.id
   dns_zone_name         = var.dns_zone_name
   tags = concat(
-    tolist(google_compute_firewall.allow_ssh.target_tags),
     tolist(google_compute_firewall.allow_https.target_tags),
-    tolist(google_compute_firewall.allow_inter_node.target_tags),
   )
 
   ip_address            = var.infinity_ip_address
@@ -39,36 +37,3 @@ module "gcp-infinity-manager" {
   enable_analytics      = var.infinity_enable_analytics
   contact_email_address = var.infinity_contact_email_address
 }
-
-# module "gcp-infinity-node" {
-#   source                = "../example/modules/gcp-infinity-node"
-#   count                 = var.infinity_node_count
-#   index                 = count.index + 1
-#   vm_image_name         = var.vm_image_node_name
-#   machine_type          = var.infinity_node_machine_type
-#   cpu_platform          = var.infinity_node_cpu_platform
-#   environment           = var.environment
-#   project_id            = var.project_id
-#   location              = var.location
-#   service_account_email = google_service_account.infinity-sa.email
-#   private_network_id    = data.google_compute_network.default.id
-#   private_subnetwork_id = data.google_compute_subnetwork.default.id
-#   dns_zone_name         = var.dns_zone_name
-#   tags = concat(
-#     tolist(google_compute_firewall.allow_ssh.target_tags),
-#     tolist(google_compute_firewall.allow_https.target_tags),
-#     tolist(google_compute_firewall.allow_inter_node.target_tags)
-#   )
-#   gateway         = data.google_compute_subnetwork.default.gateway_address
-#   subnetwork_mask = "255.255.255.255" // Use /32 for single IP address for GCP
-#   password        = var.infinity_password
-#   node_type       = "CONFERENCING"
-#   system_location = pexip_infinity_system_location.example-location-1.id
-#   tls_certificate = module.gcp-infinity-manager.manager_cert.id
-#   mgr_public_ip   = module.gcp-infinity-manager.mgr-public-ip
-#   web_password    = var.infinity_password
-#
-#   depends_on = [
-#     module.gcp-infinity-manager
-#   ]
-# }
